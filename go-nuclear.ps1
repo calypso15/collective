e2e49dd6a0e5290163c5d070c61b705f059b731d
Ryan O'Grady <ryan.ogrady@sentinelone.com>
2022-10-21 10:40:44 -0400

    Added basic output and bootstrap update

--- go-nuclear.ps1
+++ go-nuclear.ps1
@@ -1,25 +1,64 @@
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Install chocolatey
+if ((Get-Command 'choco.exe' -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Host('Installing chocolatey...')
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install important tools with Chocolatey
-Invoke-Command -ScriptBlock {
-    choco install git --yes
-    choco install python --yes
+    if ((Get-Command 'choco.exe' -ErrorAction SilentlyContinue) -eq $null) {
+        throw 'Failed to install chocolatey, aborting.'
+    }
+}
+
+# Install git
+if ((Get-Command 'git.exe' -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Host('Installing git...')
+    Invoke-Command -ScriptBlock {
+        choco install git --yes
+    }
+
+    if ((Get-Command 'git.exe' -ErrorAction SilentlyContinue) -eq $null) {
+        throw 'Failed to install git, aborting.'
+    }
+}
+
+# Install python
+if ((Get-Command 'python.exe' -ErrorAction SilentlyContinue) -eq $null) {
+    Write-Host('Installing python...')
+    Invoke-Command -ScriptBlock {
+        choco install python --yes
+    }
+
+    if ((Get-Command 'python.exe' -ErrorAction SilentlyContinue) -eq $null) {
+        throw 'Failed to install git, aborting.'
+    }
 }
 
 # Clone the go-nuclear repo if it doesn't already exist
-if (!Test-Path -Path $HOME/go-nuclear) {
+if (-Not (Test-Path -Path $HOME/go-nuclear)) {
+    Write-Host('Cloning go-nuclear repo...')
     Invoke-Command -ScriptBlock {
         git clone https://github.com/calypso15/go-nuclear.git
+    }
+
+    if (-Not (Test-Path -Path $HOME/go-nuclear)) {
+        throw 'Failed to clone go-nuclear repo, aborting.'
     }
 }
 
 Set-Location $HOME/go-nuclear
 
+Write-Host('Updating go-nuclear repo...')
 Invoke-Command -ScriptBlock {
     git pull
+}
+
+# Check for updated script
+if(Compare-Object -ReferenceObject $(Get-Content $HOME/go-nuclear/go-nuclear.ps1) -DifferenceObject $(Get-Content $MyInvocation.MyCommand.Path)) {
+    Write-Host('Updating go-nuclear bootstrap script...')
+    Copy-Item $HOME/go-nuclear/go-nuclear.ps1 $MyInvocation.MyCommand.Path
+    &$MyInvocation.MyCommand.Path
+    exit
 }
 
 Invoke-Command -ScriptBlock {
@@ -27,4 +66,4 @@ Invoke-Command -ScriptBlock {
     python check-requirements.py
 }
 
-Write-Output "TEST"
+Write-Host 'Test1'
