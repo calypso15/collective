4aae2663f63186b70301a9d5a5ee41a45d098ebb
Ryan O'Grady <ryan.ogrady@sentinelone.com>
2022-10-21 10:10:06 -0400

    Changed to using chocolatey from winget

--- go-nuclear.ps1
+++ go-nuclear.ps1
@@ -1,16 +1,26 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install important tools with Chocolatey
 Invoke-Command -ScriptBlock {
-    winget install python --accept-package-agreements --accept-source-agreements
-    winget install git.git --accept-package-agreements --accept-source-agreements
+    choco install git --yes
+    choco install python --yes
 }
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-Set-Location $HOME
-
-Invoke-Command -ScriptBlock {
-    git clone https://github.com/calypso15/go-nuclear.git
+# Clone the go-nuclear repo if it doesn't already exist
+if (!Test-Path -Path $HOME/go-nuclear) {
+    Invoke-Command -ScriptBlock {
+        git clone https://github.com/calypso15/go-nuclear.git
+    }
 }
 
 Set-Location $HOME/go-nuclear
+
+Invoke-Command -ScriptBlock {
+    git pull
+}
 
 Invoke-Command -ScriptBlock {
     pip install -r requirements.txt
