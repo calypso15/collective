d52fe7f399ba785dbe9940fc1ff74aa0eceefbb2
Ryan O'Grady <ryan.ogrady@sentinelone.com>
2022-11-16 11:09:57 -0500

    Updated to use manifest for import step

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,5 @@
 import glob
+import json
 import os
 import subprocess
 
@@ -20,42 +21,55 @@ def run_powershell(cmd):
     return completed
 
 if __name__ == '__main__':
-    home = os.path.expanduser('~')
+    HOME = os.path.expanduser('~')
+    DOWNLOAD_DIR = os.path.join(HOME, 'Documents', '.vcloud')
+    VM_DIR = os.path.join(HOME, 'Documents/Virtual Machines/S1')
+    VMNETLIB64_PATH = "C:/Program Files (x86)/VMware/VMware Workstation/vnetlib64.exe"
+    VMRUN_PATH = "C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe"
+    OVFTOOL_PATH = "C:/Program Files (x86)/VMware/VMware Workstation/OVFTool/ovftool.exe"
 
     system_requirements.check_requirements()
     vcloud_files.download_files()
 
     make_dirs()
 
+    manifest = {}
+    with open(os.path.join(DOWNLOAD_DIR, 'manifest.json')) as f:
+        manifest = json.loads(f.read())
+
     print('Excluding malware directory from Windows Defender...')
     run_powershell('Set-MpPreference -ExclusionPath $HOME/Desktop/Malware')
 
     print('Configuring vmnet...')
-    subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vnetlib64.exe" -- set vnet vmnet8 addr 192.168.192.0', shell=True)
+    subprocess.run(f'"{VMNETLIB64_PATH}" -- set vnet vmnet8 addr 192.168.192.0', shell=True)
 
     print('Deleting old VMs...')
-    files = glob.glob(os.path.join(home, 'Documents/Virtual Machines/S1', '**/*.vmx'), recursive=True)
+    files = glob.glob(os.path.join(VM_DIR, '**/*.vmx'), recursive=True)
     for file in files:
         print(f'Stopping {file}...')
-        subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe" -T ws stop "{file}" hard', shell=True)
+        subprocess.run(f'"{VMRUN_PATH}" -T ws stop "{file}" hard', shell=True)
 
         print(f'Deleting {file}...')
-        subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe" -T ws deleteVM "{file}"', shell=True)
+        subprocess.run(f'"{VMRUN_PATH} -T ws deleteVM "{file}"', shell=True)
 
     print('Installing new VMs...')
-    files = glob.glob(os.path.join(home, 'Documents/.vcloud', '**/*.ova'), recursive=True)
-    for file in files:
-        print(f'Installing {file}...')
-        subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/OVFTool/ovftool.exe" --allowExtraConfig --net:"custom=vmnet8" -o "{file}" "{os.path.join(home, "Documents/Virtual Machines/S1")}"', shell=True)
+    for file in manifest['files']:
+        name = file['name']
+        install = file['import']
+
+        if install:
+            full_name = os.path.join(DOWNLOAD_DIR, name)
+            print(f'Installing {file}...')
+            subprocess.run(f'"{OVFTOOL_PATH}" --allowExtraConfig --net:"custom=vmnet8" -o "{full_name}" "{VM_DIR}"', shell=True)
 
     print('Starting VMs...')
-    files = glob.glob(os.path.join(home, 'Documents/Virtual Machines/S1', '**/*.vmx'), recursive=True)
+    files = glob.glob(os.path.join(VM_DIR, '**/*.vmx'), recursive=True)
     for file in files:
         print(f'Starting {file}...')
-        subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe" -T ws start "{file}"', shell=True)
+        subprocess.run(f'"{VMRUN_PATH}" -T ws start "{file}"', shell=True)
 
-        p = subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe" -T ws getGuestIPAddress "{file}" -wait', shell=True, capture_output=True)
+        p = subprocess.run(f'"{VMRUN_PATH}" -T ws getGuestIPAddress "{file}" -wait', shell=True, capture_output=True)
         print(f'...Machine is up with IP address {p.stdout.decode().rstrip()}')
 
         print(f'Disabling shared folders for {file}...')
-        subprocess.run(f'"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe" -T ws disableSharedFolders "{file}"', shell=True)
+        subprocess.run(f'"{VMRUN_PATH}" -T ws disableSharedFolders "{file}"', shell=True)
