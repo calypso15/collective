66c502d24ae12e6cd20c93428f9c24e29e38894d
calypso15 <ryanjo@gmail.com>
2023-08-28 12:21:48 -0400

    Initial refactor for post-install setup

--- python/setup.py
+++ python/setup.py
@@ -5,9 +5,12 @@ import datetime
 import glob
 import json
 import os
+import requests
 import shutil
 import subprocess
 import sys
+import tempfile
+import traceback
 import winreg
 
 import vcloud_files
@@ -28,8 +31,224 @@ class Tee(object):
             f.flush()
 
 
+HOME = os.path.expanduser("~")
+DOCUMENTS_DIR = os.path.join(HOME, "Documents")
+DOWNLOAD_DIR = os.path.join(DOCUMENTS_DIR, ".vcloud")
+VM_DIR = os.path.join(HOME, "Documents/Virtual Machines/S1")
+VMWARE_DATA_DIR = r"C:/ProgramData/VMware"
+VMNETLIB64_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vnetlib64.exe"
+VMRUN_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe"
+VMWARE_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vmware.exe"
+OVFTOOL_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/OVFTool/ovftool.exe"
+
+
+def main():
+    f = open(os.path.join(DOCUMENTS_DIR, "log-python.txt"), "a")
+    sys.stdout = Tee(sys.stdout, f)
+    sys.stderr = Tee(sys.stderr, f)
+
+    print_header(f)
+    atexit.register(print_footer, f)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file")
+    args = parser.parse_args()
+
+    config = {}
+    config_file = args.config_file
+    with open(config_file) as f:
+        config = json.loads(f.read())
+
+    system_requirements.check_requirements(
+        ignore_warnings=config.get("IgnoreWarnings", False),
+        ignore_errors=config.get("IgnoreErrors", False),
+    )
+
+    print("Starting VMWare...")
+    subprocess.Popen(VMWARE_PATH, shell=True)
+
+    rv = ctypes.windll.user32.MessageBoxW(
+        0,
+        "VMWare should now be running. Please configure your license and then click OK.",
+        "Starting VMWare Workstation",
+        0x1 ^ 0x40 ^ 0x1000,
+    )
+
+    if rv != 1:
+        print("Aborting setup.")
+        sys.exit()
+
+    make_dir(os.path.join(HOME, "Desktop/Malware"))
+    print("Excluding malware directory from Windows Defender...")
+    run_powershell("Set-MpPreference -ExclusionPath $HOME/Desktop/Malware")
+
+    global AUTH, VCLOUD_URL
+    AUTH = None
+    VCLOUD_URL = config["Vcloud"]["Url"]
+
+    try:
+        AUTH = requests.auth.HTTPBasicAuth(
+            config["Vcloud"]["Username"], config["Vcloud"]["Password"]
+        )
+        r = requests.get(VCLOUD_URL, auth=AUTH)
+        r.raise_for_status()
+    except requests.ConnectionError as x:
+        print(traceback.format_exc())
+        print(
+            f"Unable to connect to {VCLOUD_URL}, exiting. Please contact ryan.ogrady@sentinelone.com for additional support."
+        )
+        sys.exit()
+    except requests.HTTPError as x:
+        print(traceback.format_exc())
+        print(
+            f"Received HTTP {x.response.status_code}, exiting. Please contact ryan.ogrady@sentinelone.com for additional support."
+        )
+        sys.exit()
+
+    vcloud_files.download_files(url=VCLOUD_URL, auth=AUTH)
+
+    rv = ctypes.windll.user32.MessageBoxW(
+        0,
+        "Do you want to install the virtual environment? This will delete the old environment (if any), and you will need to re-install agents, snapshots, etc.",
+        "Install virtual environment?",
+        0x4 ^ 0x40 ^ 0x1000,
+    )
+
+    if rv == 6:
+        print("Configuring vmnet8...")
+        old_lines = []
+        with open(os.path.join(VMWARE_DATA_DIR, "vmnetnat.conf"), "r") as f:
+            old_lines = f.readlines()
+
+        new_lines = []
+        for l in old_lines:
+            if l.startswith("ip ="):
+                new_lines.append("ip = 192.168.192.2/24\n")
+            else:
+                new_lines.append(l)
+
+        with open(os.path.join(VMWARE_DATA_DIR, "vmnetnat.conf"), "w") as f:
+            f.writelines(new_lines)
+
+        registry_key = winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            r"SOFTWARE\\WOW6432Node\\VMware, Inc.\\VMnetLib\\VMnetConfig\\vmnet8",
+            0,
+            winreg.KEY_ALL_ACCESS,
+        )
+        winreg.SetValueEx(
+            registry_key, "IPSubnetAddress", 0, winreg.REG_SZ, "192.168.192.0"
+        )
+        winreg.CloseKey(registry_key)
+
+        subprocess.run(f'"{VMNETLIB64_PATH}" -- stop nat', shell=True)
+        subprocess.run(f'"{VMNETLIB64_PATH}" -- stop dhcp', shell=True)
+        subprocess.run(f'"{VMNETLIB64_PATH}" -- start dhcp', shell=True)
+        subprocess.run(f'"{VMNETLIB64_PATH}" -- start nat', shell=True)
+
+        manifest = None
+        if os.path.exists(os.path.join(DOWNLOAD_DIR, "manifest.json")):
+            with open(os.path.join(DOWNLOAD_DIR, "manifest.json")) as f:
+                manifest = json.loads(f.read())
+
+        if manifest != None:
+            if os.path.exists(VM_DIR):
+                print("Stopping VMs...")
+                files = glob.glob(os.path.join(VM_DIR, "**/*.vmx"), recursive=True)
+                for file in files:
+                    print(f"Stopping {file}...")
+                    subprocess.run(
+                        f'"{VMRUN_PATH}" -T ws stop "{file}" hard', shell=True
+                    )
+
+                print("Deleting old VMs...")
+                shutil.rmtree(VM_DIR)
+
+            make_dir(VM_DIR)
+
+            sorted_list = sorted(
+                manifest["files"], key=lambda d: d.get("order", sys.maxsize)
+            )
+
+            print("Installing and starting new VMs...")
+            for file in sorted_list:
+                name = file["name"]
+                base_name = os.path.splitext(name)[0]
+                install = file["import"]
+
+                ova_path = os.path.join(DOWNLOAD_DIR, name)
+                vmx_path = os.path.join(VM_DIR, base_name, base_name + ".vmx")
+
+                if install:
+                    install_vm(ova_path=ova_path, vmx_path=vmx_path)
+                    setup_vm(vmx_path=vmx_path)
+
+        else:
+            print("Skipping environment setup, there was a problem with the manifest.")
+    else:
+        print("Skipping environment setup at user request.")
+
+
+def install_vm(ova_path, vmx_path):
+    print(f"Installing {ova_path}...")
+    subprocess.run(
+        f'"{OVFTOOL_PATH}" --allowExtraConfig --net:"custom=vmnet8" -o "{ova_path}" "{VM_DIR}"',
+        shell=True,
+    )
+
+    print(f"Starting {vmx_path}...")
+    subprocess.run(f'"{VMRUN_PATH}" -T ws start "{vmx_path}"', shell=True)
+
+    p = subprocess.run(
+        f'"{VMRUN_PATH}" -T ws getGuestIPAddress "{vmx_path}" -wait',
+        shell=True,
+        capture_output=True,
+    )
+    ip = p.stdout.decode().rstrip()
+    print(f"...Machine is up with IP address {ip}")
+
+
+def setup_vm(vmx_path):
+    p = subprocess.run(
+        f'"{VMRUN_PATH}" -T ws getGuestIPAddress "{vmx_path}" -wait',
+        shell=True,
+        capture_output=True,
+    )
+    ip = p.stdout.decode().rstrip()
+
+    print(f"Disabling shared folders for {vmx_path}...")
+    subprocess.run(
+        f'"{VMRUN_PATH}" -T ws disableSharedFolders "{vmx_path}"', shell=True
+    )
+
+    if ip == "192.168.192.250":
+        copy_file(
+            vmx_path=vmx_path,
+            username="kali",
+            password="kali",
+            filename="setup/kali/ResistanceIsFutile.exe",
+            guest_path="/home/kali/.msf4/local/ResistanceIsFutile.exe",
+        )
+
+
+def copy_file(vmx_path, username, password, filename, guest_path):
+    print(f"Downloading {filename}...")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        path = os.path.join(tmpdirname, filename)
+        vcloud_files.download_file(
+            url=VCLOUD_URL, auth=AUTH, filename=filename, local_path=path
+        )
+
+    print(f"Copying {filename} to {vmx_path}...")
+    p = subprocess.run(
+        f'"{VMRUN_PATH}" -T ws -gu "{username}" -gp "{password}" copyFileFromHostToGuest "{vmx_path}" "{path}" "{guest_path}"',
+        shell=True,
+        capture_output=True,
+    )
+
+
 def make_dir(name):
-    print(f'Creating directory {name}...')
+    print(f"Creating directory {name}...")
     os.makedirs(name, exist_ok=True)
 
 
@@ -39,141 +258,21 @@ def run_powershell(cmd):
 
 
 def print_header(file):
-    print('**********************', file=file)
-    print('Python transcript start', file=file)
-    print('Start time: '+datetime.datetime.today().strftime('%Y%m%d%H%M%S'), file=file)
-    print('Python version: '+sys.version, file=file)
-    print('**********************', file=file)
+    print("**********************", file=file)
+    print("Python transcript start", file=file)
+    print(
+        "Start time: " + datetime.datetime.today().strftime("%Y%m%d%H%M%S"), file=file
+    )
+    print("Python version: " + sys.version, file=file)
+    print("**********************", file=file)
 
 
 def print_footer(file):
-    print('**********************', file=file)
-    print('Python transcript end', file=file)
-    print('End time: '+datetime.datetime.today().strftime('%Y%m%d%H%M%S'), file=file)
-    print('**********************', file=file)
+    print("**********************", file=file)
+    print("Python transcript end", file=file)
+    print("End time: " + datetime.datetime.today().strftime("%Y%m%d%H%M%S"), file=file)
+    print("**********************", file=file)
 
 
-if __name__ == '__main__':
-    HOME = os.path.expanduser('~')
-    DOCUMENTS_DIR = os.path.join(HOME, 'Documents')
-    DOWNLOAD_DIR = os.path.join(DOCUMENTS_DIR, '.vcloud')
-    VM_DIR = os.path.join(HOME, 'Documents/Virtual Machines/S1')
-    VMWARE_DATA_DIR = r"C:/ProgramData/VMware"
-    VMNETLIB64_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vnetlib64.exe"
-    VMRUN_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vmrun.exe"
-    VMWARE_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/vmware.exe"
-
-    OVFTOOL_PATH = r"C:/Program Files (x86)/VMware/VMware Workstation/OVFTool/ovftool.exe"
-
-    f = open(os.path.join(DOCUMENTS_DIR, 'log-python.txt'), 'a')
-    sys.stdout = Tee(sys.stdout, f)
-    sys.stderr = Tee(sys.stderr, f)
-
-    print_header(f)
-    atexit.register(print_footer, f)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('config_file')
-    args = parser.parse_args()
-
-    config = {}
-    config_file = args.config_file
-    with open(config_file) as f:
-        config = json.loads(f.read())
-
-    system_requirements.check_requirements(ignore_warnings=config.get('IgnoreWarnings', False), ignore_errors=config.get('IgnoreErrors', False))
-
-    print('Starting VMWare...')
-    subprocess.Popen(VMWARE_PATH, shell=True)
-
-    rv = ctypes.windll.user32.MessageBoxW(0, "VMWare should now be running. Please configure your license and then click OK.", "Starting VMWare Workstation", 0x1 ^ 0x40 ^ 0x1000)
-
-    if (rv != 1):
-        print('Aborting setup.')
-        sys.exit()
-
-    make_dir(os.path.join(HOME, 'Desktop/Malware'))
-    print('Excluding malware directory from Windows Defender...')
-    run_powershell('Set-MpPreference -ExclusionPath $HOME/Desktop/Malware')
-
-    vcloud_files.download_files(config['Vcloud']['Url'], config['Vcloud']['Username'], config['Vcloud']['Password'])
-
-    rv = ctypes.windll.user32.MessageBoxW(0, "Do you want to install the virtual environment? This will delete the old environment (if any), and you will need to re-install agents, snapshots, etc.", "Install virtual environment?", 0x4 ^ 0x40 ^ 0x1000)
-
-    if (rv == 6):
-        print('Configuring vmnet8...')
-        old_lines = []
-        with open(os.path.join(VMWARE_DATA_DIR, 'vmnetnat.conf'), 'r') as f:
-            old_lines = f.readlines()
-
-        new_lines = []
-        for l in old_lines:
-            if l.startswith('ip ='):
-                new_lines.append('ip = 192.168.192.2/24\n')
-            else:
-                new_lines.append(l)
-
-        with open(os.path.join(VMWARE_DATA_DIR, 'vmnetnat.conf'), 'w') as f:
-            f.writelines(new_lines)
-
-        registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\\WOW6432Node\\VMware, Inc.\\VMnetLib\\VMnetConfig\\vmnet8', 0, winreg.KEY_ALL_ACCESS)
-        winreg.SetValueEx(registry_key, 'IPSubnetAddress', 0, winreg.REG_SZ, '192.168.192.0')
-        winreg.CloseKey(registry_key)
-
-        subprocess.run(f'"{VMNETLIB64_PATH}" -- stop nat', shell=True)
-        subprocess.run(f'"{VMNETLIB64_PATH}" -- stop dhcp', shell=True)
-        subprocess.run(f'"{VMNETLIB64_PATH}" -- start dhcp', shell=True)
-        subprocess.run(f'"{VMNETLIB64_PATH}" -- start nat', shell=True)
-
-        manifest = None
-        if os.path.exists(os.path.join(DOWNLOAD_DIR, 'manifest.json')):
-            with open(os.path.join(DOWNLOAD_DIR, 'manifest.json')) as f:
-                manifest = json.loads(f.read())
-
-        if manifest != None:
-            if os.path.exists(VM_DIR):
-                print('Stopping VMs...')
-                files = glob.glob(os.path.join(VM_DIR, '**/*.vmx'), recursive=True)
-                for file in files:
-                    print(f'Stopping {file}...')
-                    subprocess.run(f'"{VMRUN_PATH}" -T ws stop "{file}" hard', shell=True)
-
-                print('Deleting old VMs...')
-                shutil.rmtree(VM_DIR)
-
-            make_dir(VM_DIR)
-
-            sorted_list = sorted(manifest['files'], key=lambda d: d.get('order', sys.maxsize))
-
-            print('Installing and starting new VMs...')
-            for file in sorted_list:
-                name = file['name']
-                base_name = os.path.splitext(name)[0]
-                install = file['import']
-
-                ova_path = os.path.join(DOWNLOAD_DIR, name)
-                vmx_path = os.path.join(VM_DIR, base_name, base_name+'.vmx')
-
-                if install:
-                    print(f'Installing {ova_path}...')
-                    subprocess.run(
-                        f'"{OVFTOOL_PATH}" --allowExtraConfig --net:"custom=vmnet8" -o "{ova_path}" "{VM_DIR}"', shell=True)
-
-                    print(f'Starting {vmx_path}...')
-                    subprocess.run(f'"{VMRUN_PATH}" -T ws start "{vmx_path}"', shell=True)
-
-                    p = subprocess.run(f'"{VMRUN_PATH}" -T ws getGuestIPAddress "{vmx_path}" -wait',
-                                    shell=True, capture_output=True)
-                    ip = p.stdout.decode().rstrip()
-                    print(f'...Machine is up with IP address {ip}')
-
-                    print(f'Disabling shared folders for {vmx_path}...')
-                    subprocess.run(f'"{VMRUN_PATH}" -T ws disableSharedFolders "{vmx_path}"', shell=True)
-
-                    if "192.168.192.10" == ip:
-                        print(f'Installing DC rename scripts for {vmx_path}...')
-                        subprocess.run(f'"{VMRUN_PATH}" -T ws -gu "{config["VM"]["Username"]}" -gp "{config["VM"]["Password"]}" copyFileFromHostToGuest "{vmx_path}" ../bat/Rename-DC.bat C:/Rename-DC.bat', shell=True)
-        else:
-            print('Skipping environment setup, there was a problem with the manifest.')
-    else:
-        print('Skipping environment setup at user request.')
+if __name__ == "__main__":
+    main()
--- python/vcloud_files.py
+++ python/vcloud_files.py
@@ -11,130 +11,120 @@ import traceback
 import urllib
 
 
-def download_file(filename, path = None):
-    if path == None:
-        path = DOWNLOAD_DIR
+def download_file(url, auth, filename, local_path=None):
+    if local_path == None:
+        local_path = DOWNLOAD_DIR
 
-    url = urllib.parse.urljoin(VCLOUD_URL, filename)
-    datafile = os.path.join(path, filename)
+    url = urllib.parse.urljoin(url, filename)
+    datafile = os.path.join(local_path, filename)
 
     local_filename = urllib.parse.unquote(datafile)
 
-    with requests.get(url, auth=AUTH, stream=True) as r:
+    with requests.get(url, auth=auth, stream=True) as r:
         r.raise_for_status()
-        chunk_size = 1024*1024
-        total_size = int(r.headers.get('content-length', 0))
+        chunk_size = 1024 * 1024
+        total_size = int(r.headers.get("content-length", 0))
 
-        with open(local_filename, 'wb') as f:
+        with open(local_filename, "wb") as f:
             current_size = 0
 
             for chunk in r.iter_content(chunk_size=chunk_size):
                 current_size += len(chunk)
-                progress = round(100*current_size/total_size, 2)
+                progress = round(100 * current_size / total_size, 2)
                 print(
-                    f'\rDownloading {url}... {current_size//(1024*1024)} MB / {total_size//(1024*1024)} MB, {progress}%', end='')
+                    f"\rDownloading {url}... {current_size//(1024*1024)} MB / {total_size//(1024*1024)} MB, {progress}%",
+                    end="",
+                )
                 f.write(chunk)
 
-            print('')
+            print("")
 
     return local_filename
 
 
-def check_hash(filename, hash_value, hash_type='sha256'):
+def check_hash(filename, hash_value, hash_type="sha256"):
     datafile = os.path.join(DOWNLOAD_DIR, filename)
 
-    if (not os.path.isfile(datafile)):
+    if not os.path.isfile(datafile):
         return False
 
     alg = hashlib.new(hash_type)
-    with open(datafile, 'rb') as f:
+    with open(datafile, "rb") as f:
         for byte_block in iter(lambda: f.read(2**16), b""):
             alg.update(byte_block)
 
-    return (alg.hexdigest() == hash_value)
+    return alg.hexdigest() == hash_value
 
 
-def download_files(vcloud_url, vcloud_user, vcloud_pass):
-    global AUTH
+def download_files(url, auth):
     global TEMP_DIR, DOWNLOAD_DIR
-    global VCLOUD_URL, VCLOUD_USER, VCLOUD_PASS
-
-    VCLOUD_URL = vcloud_url
-    VCLOUD_USER = vcloud_user
-    VCLOUD_PASS = vcloud_pass
-
-    try:
-        AUTH = requests.auth.HTTPBasicAuth(vcloud_user, vcloud_pass)
-        r = requests.get(vcloud_url, auth=AUTH)
-        r.raise_for_status()
-    except requests.ConnectionError as x:
-        print(traceback.format_exc())
-        print(f"Unable to connect to {vcloud_url}, exiting. Please contact ryan.ogrady@sentinelone.com for additional support.")
-        sys.exit()
-    except requests.HTTPError as x:
-        print(traceback.format_exc())
-        print(f"Received HTTP {x.response.status_code}, exiting. Please contact ryan.ogrady@sentinelone.com for additional support.")
-        sys.exit()
 
     TEMP_DIR = tempfile.gettempdir()
-    DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), 'Documents', '.vcloud')
+    DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), "Documents", ".vcloud")
 
     if not os.path.exists(DOWNLOAD_DIR):
         os.makedirs(DOWNLOAD_DIR)
 
     manifest = None
     with tempfile.TemporaryDirectory() as tmpdirname:
-        download_file('manifest.json', tmpdirname)
-        with open(os.path.join(tmpdirname, 'manifest.json')) as f:
+        download_file(url, auth, "manifest.json", tmpdirname)
+        with open(os.path.join(tmpdirname, "manifest.json")) as f:
             manifest = json.loads(f.read())
 
-    if os.path.exists(os.path.join(DOWNLOAD_DIR, 'manifest.json')):
+    if os.path.exists(os.path.join(DOWNLOAD_DIR, "manifest.json")):
         old_manifest = None
-        with open(os.path.join(DOWNLOAD_DIR, 'manifest.json')) as f:
+        with open(os.path.join(DOWNLOAD_DIR, "manifest.json")) as f:
             old_manifest = json.loads(f.read())
 
-        if manifest['version'] > old_manifest['version']:
-            rv = ctypes.windll.user32.MessageBoxW(0, f"There is a newer version (v{manifest['version']}) of the virtual environment. Do you want to download it?", "Download virtual environment?", 0x4 ^ 0x40 ^ 0x1000)
+        if manifest["version"] > old_manifest["version"]:
+            rv = ctypes.windll.user32.MessageBoxW(
+                0,
+                f"There is a newer version (v{manifest['version']}) of the virtual environment. Do you want to download it?",
+                "Download virtual environment?",
+                0x4 ^ 0x40 ^ 0x1000,
+            )
 
-            if (rv != 6):
-                print('Skipping environment download at user request.')
+            if rv != 6:
+                print("Skipping environment download at user request.")
                 manifest = None
         else:
-            print('Skipping environment download, environment is up-to-date.')
+            print("Skipping environment download, environment is up-to-date.")
             manifest = None
 
     if manifest != None:
-        print('Downloading OVAs...')
-        sorted_list = sorted(manifest['files'], key=lambda d: d.get('order', sys.maxsize))
+        print("Downloading OVAs...")
+        sorted_list = sorted(
+            manifest["files"], key=lambda d: d.get("order", sys.maxsize)
+        )
         for file in sorted_list:
             try:
-                name = file['name']
-                hash_type = file['hash_type']
-                hash_value = file['hash_value']
+                name = file["name"]
+                hash_type = file["hash_type"]
+                hash_value = file["hash_value"]
 
-                print(f"Checking hash of '{name}'...", end='')
+                print(f"Checking hash of '{name}'...", end="")
                 sys.stdout.flush()
 
-                if (not os.path.isfile(os.path.join(DOWNLOAD_DIR, name))):
-                    print('file missing.')
+                if not os.path.isfile(os.path.join(DOWNLOAD_DIR, name)):
+                    print("file missing.")
                     download_file(name)
-                elif (not check_hash(name, hash_value, hash_type=hash_type)):
-                    print('does not match.')
+                elif not check_hash(name, hash_value, hash_type=hash_type):
+                    print("does not match.")
                     download_file(name)
                 else:
-                    print('matches.')
+                    print("matches.")
             except Exception as e:
                 print(e)
 
-        with open(os.path.join(DOWNLOAD_DIR, 'manifest.json'), 'w') as f:
+        with open(os.path.join(DOWNLOAD_DIR, "manifest.json"), "w") as f:
             f.write(json.dumps(manifest, indent=4))
 
-        print('Finished downloading OVAs.')
+        print("Finished downloading OVAs.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('config_file')
+    parser.add_argument("config_file")
     args = parser.parse_args()
 
     config = {}
@@ -142,4 +132,8 @@ if __name__ == '__main__':
     with open(config_file) as f:
         config = json.loads(f.read())
 
-    download_files(config['Vcloud']['Url'], config['Vcloud']['Username'], config['Vcloud']['Password'])
+    download_files(
+        config["Vcloud"]["Url"],
+        config["Vcloud"]["Username"],
+        config["Vcloud"]["Password"],
+    )
