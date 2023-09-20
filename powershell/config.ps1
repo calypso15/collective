64e32622320d7c06d437ce5cc351aa4553624c67
calypso15 <ryanjo@gmail.com>
2023-09-20 14:58:36 -0400

    Fixed label overlap

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -117,6 +117,7 @@ $SiteTokenLabel.width            = 25
 $SiteTokenLabel.height           = 10
 $SiteTokenLabel.location         = New-Object System.Drawing.Point(16,272)
 $SiteTokenLabel.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$SiteTokenLabel.BackColor        = [System.Drawing.ColorTranslator]::FromHtml("transparent")
 
 $SiteToken                       = New-Object system.Windows.Forms.TextBox
 $SiteToken.multiline             = $false
