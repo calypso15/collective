993e4b02b810925e5d677778869f99792427fac4
calypso15 <ryanjo@gmail.com>
2023-09-22 10:35:36 -0400

    Debugging json error

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -252,7 +252,7 @@ function ShowDialog($FilePath)
         $result["IgnoreErrors"] = $IgnoreErrors.Checked
         $result["NonInteractive"] = $NonInteractive.Checked
 
-        ConvertTo-Json -Depth 5 -InputObject $result | Out-File $FilePath -Encoding "Unicode"
+        ConvertTo-Json -Depth 5 -InputObject $result | Out-File $FilePath -Encoding "ASCII"
         $ConfigUI.DialogResult = [System.Windows.Forms.DialogResult]::OK
     })
 
