3411000523e7efbd7369f92f6c4f0c14994a1140
calypso15 <ryanjo@gmail.com>
2023-09-25 12:10:08 -0400

    Lowered disk space requirements in system check

--- python/system_requirements.py
+++ python/system_requirements.py
@@ -10,104 +10,124 @@ import sys
 from enum import IntEnum
 from subprocess import getoutput
 
+
 class State(IntEnum):
     PASS = 1
     WARN = 2
     FAIL = 4
     FATAL = 8
 
+
 def is_windows() -> bool:
-    return platform.system() == 'Windows'
+    return platform.system() == "Windows"
+
 
 def is_admin() -> bool:
     try:
-        temp = os.listdir(os.sep.join([os.environ.get('SystemRoot','C:\\windows'),'temp']))
+        temp = os.listdir(
+            os.sep.join([os.environ.get("SystemRoot", "C:\\windows"), "temp"])
+        )
     except:
         return False
     else:
         return True
 
+
 def get_cpu_name() -> str:
-    return getoutput('wmic cpu get name')
+    return getoutput("wmic cpu get name")
+
 
 def get_free_diskspace() -> int:
-    return shutil.disk_usage('.').free
+    return shutil.disk_usage(".").free
+
 
 def get_total_memory() -> int:
     return psutil.virtual_memory().total
+
 
 def check() -> str:
     state = State.PASS
 
     if is_windows():
-        print('System is running Windows...PASS.')
+        print("System is running Windows...PASS.")
     else:
-        print('System is running Windows...FATAL!')
+        print("System is running Windows...FATAL!")
         return State.FATAL
 
     if is_admin():
-        print('Script is running as admin...PASS.')
+        print("Script is running as admin...PASS.")
     else:
-        print('Script is running as admin...FAIL!')
+        print("Script is running as admin...FAIL!")
         state = state | State.FAIL
 
-    test = ''.join(get_cpu_name().split('\n')[1:]).strip()
-    if 'i7' in test or 'i9' in test:
-        print('System processor is Intel Core i7 or i9...PASS.')
+    test = "".join(get_cpu_name().split("\n")[1:]).strip()
+    if "i7" in test or "i9" in test:
+        print("System processor is Intel Core i7 or i9...PASS.")
     else:
-        print('System processor is Intel Core i7 or i9...WARN.')
-        print(f'  System reports processor name \'{test}\'')
+        print("System processor is Intel Core i7 or i9...WARN.")
+        print(f"  System reports processor name '{test}'")
         state = state | State.WARN
 
-    test = -(get_free_diskspace()//-(2**30))
-    if test >= 500:
-        print('Disk has at least 500GB of free space...PASS.')
+    test = -(get_free_diskspace() // -(2**30))
+    if test >= 250:
+        print("Disk has at least 250GB of free space...PASS.")
+    elif test >= 150:
+        print("Disk has at least 250GB of free space...WARN!")
+        print(f"  System reports {test}GB of free space.")
+        state = state | State.WARN
     else:
-        print('Disk has at least 500GB of free space...FAIL!')
-        print(f'  System reports {test}GB of free space.')
+        print("Disk has at least 250GB of free space...FAIL!")
+        print(f"  System reports {test}GB of free space.")
         state = state | State.FAIL
 
-    test = -(get_total_memory()//-(2**30))
+    test = -(get_total_memory() // -(2**30))
     if test >= 64:
-        print('System has at least 64GB of memory...PASS.')
+        print("System has at least 64GB of memory...PASS.")
     elif test >= 32:
-        print('System has at least 64GB of memory...WARN.')
-        print(f'  System reports {test}GB of memory.')
+        print("System has at least 64GB of memory...WARN.")
+        print(f"  System reports {test}GB of memory.")
         state = state | State.WARN
     else:
-        print('System has at least 64GB of memory...FAIL!')
-        print(f'  System reports {test}GB of memory.')
+        print("System has at least 64GB of memory...FAIL!")
+        print(f"  System reports {test}GB of memory.")
         state = state | State.FAIL
 
+    return State(2 ** math.floor(math.log2(state)))
 
-    return State(2**math.floor(math.log2(state)))
 
 def check_requirements(ignore_warnings=False, ignore_errors=False):
-    print('Checking system requirements...')
+    print("Checking system requirements...")
     result = check()
-    print('')
+    print("")
 
-    if(result == State.PASS):
-        print('This system meets all requirements.')
-    elif(result == State.WARN):
-        if(ignore_warnings):
-            print('This system may be insufficient, but IgnoreWarnings is set to true.')
+    if result == State.PASS:
+        print("This system meets all requirements.")
+    elif result == State.WARN:
+        if ignore_warnings:
+            print("This system may be insufficient, but IgnoreWarnings is set to true.")
         else:
-            print('This system may be insufficient. To proceed anyway, change \'"IgnoreWarnings": false\' in the config file to \'"IgnoreWarnings": true\' and re-run setup.')
+            print(
+                "This system may be insufficient. To proceed anyway, change '\"IgnoreWarnings\": false' in the config file to '\"IgnoreWarnings\": true' and re-run setup."
+            )
             sys.exit(1)
-    elif(result == State.FAIL):
-        if(ignore_errors):
-            print('This system does not meet the minimum requirements, but IgnoreErrors is set to true.')
+    elif result == State.FAIL:
+        if ignore_errors:
+            print(
+                "This system does not meet the minimum requirements, but IgnoreErrors is set to true."
+            )
         else:
-            print('This system does not meet the minimum requirements, aborting. To proceed anyway, change \'"IgnoreErrors": false\' in the config file to \'"IgnoreErrors": true\' and re-run setup.')
+            print(
+                "This system does not meet the minimum requirements, aborting. To proceed anyway, change '\"IgnoreErrors\": false' in the config file to '\"IgnoreErrors\": true' and re-run setup."
+            )
             sys.exit(1)
     else:
-        print('This system does not meet the minimum requirements, aborting.')
+        print("This system does not meet the minimum requirements, aborting.")
         sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('config_file')
+    parser.add_argument("config_file")
     args = parser.parse_args()
 
     config = {}
@@ -115,4 +135,7 @@ if __name__ == '__main__':
     with open(config_file) as f:
         config = json.loads(f.read())
 
-    check_requirements(ignore_warnings=config.get('IgnoreWarnings', False), ignore_errors=config.get('IgnoreErrors', False))
+    check_requirements(
+        ignore_warnings=config.get("IgnoreWarnings", False),
+        ignore_errors=config.get("IgnoreErrors", False),
+    )
