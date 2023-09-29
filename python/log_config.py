3889578dc5a42444633f863df0788322d4296eee
calypso15 <ryanjo@gmail.com>
2023-09-29 09:14:31 -0400

    Changing format and level for console and file handlers

--- python/log_config.py
+++ python/log_config.py
@@ -5,8 +5,17 @@ HOME = os.path.expanduser("~")
 DOCUMENTS_DIR = os.path.join(HOME, "Documents")
 LOG_FILENAME = os.path.join(DOCUMENTS_DIR, "log-python.txt")
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler(LOG_FILENAME, "a", "utf-8"), logging.StreamHandler()],
-)
+console_format = logging.Formatter("%(levelname)s: %(message)s")
+file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(console_format)
+
+file_handler = logging.FileHandler(LOG_FILENAME, "a", "utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(file_format)
+
+logging.getLogger().addHandler(console_handler)
+logging.getLogger().addHandler(file_handler)
+logging.getLogger().setLevel(logging.DEBUG)
