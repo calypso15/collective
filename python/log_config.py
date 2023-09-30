0443472d5eff0ece0f3b7d49a87483028ce1fe84
calypso15 <ryanjo@gmail.com>
2023-09-30 17:07:51 -0400

    Fixing log format

--- python/log_config.py
+++ python/log_config.py
@@ -6,9 +6,7 @@ DOCUMENTS_DIR = os.path.join(HOME, "Documents")
 LOG_FILENAME = os.path.join(DOCUMENTS_DIR, "log-python.txt")
 
 console_format = logging.Formatter("%(levelname)s: %(message)s")
-file_format = logging.Formatter(
-    "%(asctime)s - %(filename)s - %(levelname)s - %(message)s"
-)
+file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
